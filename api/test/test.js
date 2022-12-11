dc81d037cda429389e5d9c7bab4fc979ff441e1a
Fabien Lapert <flapert@feelagile.com>
2022-12-11 18:04:46 +0100

    Updating test

--- api/test/test.js
+++ api/test/test.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
 const app = require('../index.js');
+const request = require('supertest').agent(app.listen());
 
 // ------------------- MATERIAL -------------------
 
